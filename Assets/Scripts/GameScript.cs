f8a415311e43612f51fafc77f7ced36fb36c54b7
Josh Maggard <jmaggard62@gmail.com>
2014-04-15 21:15:10 -0500

    First commit

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+        FutileParams fparams = new FutileParams(true, false, false, false);
+        fparams.AddResolutionLevel(480.0f, 1.0f, 1.0f, "");
+        fparams.backgroundColor = Color.black;
+        fparams.origin = new Vector2(.5f, .5f);
+        Futile.instance.Init(fparams);
+
+        Futile.atlasManager.LoadAtlas("Atlases/gameAtlas");
+
+        Futile.stage.AddChild(new Player());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+	public class Player : FSprite
+	{
+        float speed = 200.0f;
+        public Player() : base("character")
+        {
+            
+        }
+
+        public override void HandleAddedToStage()
+        {
+            Futile.instance.SignalUpdate += Update;
+            base.HandleAddedToStage();
+        }
+
+        public override void HandleRemovedFromStage()
+        {
+            Futile.instance.SignalUpdate -= Update;
+            base.HandleRemovedFromStage();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(UnityEngine.KeyCode.W))
+                this.y += Time.deltaTime * speed;
+            else if (Input.GetKey(UnityEngine.KeyCode.S))
+                this.y -= Time.deltaTime * speed;
+
+            if (Input.GetKey(KeyCode.A))
+                this.x -= Time.deltaTime * speed;
+            else
+                if (Input.GetKey(KeyCode.D))
+                    this.x += Time.deltaTime * speed;
+        }
+
+	}

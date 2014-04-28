66ab988478be9e86096c08b0b1fcc267557af44a
Josh Maggard <jmaggard62@gmail.com>
2014-04-28 14:25:41 -0500

    More maps stuff

--- Assets/Scripts/C.cs
+++ Assets/Scripts/C.cs
@@ -11,8 +11,8 @@ public static class C
     public const string SmallFont = "PressStart2Psmall";
     public const float disableMovementTime = .5f;
 
-    public const string startMap = "room1_1";
-    public const string startDoor = "tutorialDoor";
+    public const string startMap = "tutorial";
+    public const string startDoor = "startDoor";
 
     public static bool transitioning = false;
     public const float transitioningTime = .5f;

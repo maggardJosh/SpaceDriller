c90e7a789a137da1d75a65377ed4b54a715d2567
Josh Maggard <jmaggard62@gmail.com>
2014-04-28 02:39:49 -0500

    door particles

--- Assets/Scripts/GameObjects/Wall.cs
+++ Assets/Scripts/GameObjects/Wall.cs
@@ -10,7 +10,7 @@ public class Wall : BaseGameObject
     int level;
     bool isBroken = false;
     int doorNumber;
-    
+
     public Wall(Vector2 pos, int level, int doorNumber)
     {
         this.doorNumber = doorNumber;
@@ -20,7 +20,7 @@ public class Wall : BaseGameObject
         sprite.addAnimation(new FAnimation("normal", new int[] { 1 }, 100));
         sprite.addAnimation(new FAnimation("broken", new int[] { 2 }, 100));
         this.AddChild(sprite);
-        
+
     }
 
     protected override void Update()
@@ -45,26 +45,47 @@ public class Wall : BaseGameObject
                     playerRelativePos.y > -sprite.height / 2 &&
                     playerRelativePos.y < sprite.height / 2)
                 {
-                  
+
                     if (playerRelativePos.x > 0)
-                        world.p.x = this.x - sprite.width/2;
+                        world.p.x = this.x - sprite.width / 2;
                     else
-                        world.p.x = this.x + sprite.width/2;
-                    
+                        world.p.x = this.x + sprite.width / 2;
+
                 }
-                
+
             }
         }
         base.Update();
     }
     public void breakDoor()
     {
-        RXDebug.Log("HI");
         isBroken = true;
         sprite.play("broken");
     }
     protected override void die()
     {
+        FParticleSystem rubbleParticleSystem;
+        FParticleDefinition rubbleParticleDefinition;
+        
+        rubbleParticleSystem = new FParticleSystem(80);
+        this.AddChild(rubbleParticleSystem);
+        rubbleParticleDefinition = new FParticleDefinition(C.whiteElement);
+        rubbleParticleDefinition.startColor = Color.white;
+        rubbleParticleDefinition.startScale = 1;
+        rubbleParticleDefinition.endScale = 0;
+        rubbleParticleDefinition.endColor = new Color(0, 0, 0, 0);
+        rubbleParticleDefinition.lifetime = 4;
+        rubbleParticleSystem.accelY = -300;
+        for (int i = 0; i < 80; i++)
+        {
+            rubbleParticleDefinition.SetElementByName("doorParticle" + level + "_" + (RXRandom.Int(3) + 1));
+            rubbleParticleDefinition.x = RXRandom.Float() * sprite.width - sprite.width / 2;
+            rubbleParticleDefinition.y = RXRandom.Float() * sprite.height - sprite.height / 2;
+            float angle = 180 * RXRandom.Float();
+            rubbleParticleDefinition.speedX = Mathf.Cos(angle * C.PIOVER180) * (75 + 25 * RXRandom.Float());
+            rubbleParticleDefinition.speedY = Mathf.Sin(angle * C.PIOVER180) * (75 + 25 * RXRandom.Float());
+            rubbleParticleSystem.AddParticle(rubbleParticleDefinition);
+        }
         C.doorsBroken.Add(new KeyValuePair<string, int>(world.map.mapName, doorNumber));
         //Particles
         breakDoor();
